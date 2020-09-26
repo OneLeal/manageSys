1fce033949f1bf51cabac63ca7834fc2b0ed110d
浅芷初夏 <55690294+OneLeal@users.noreply.github.com>
2020-09-26 20:33:50 +0800

    fix: 修改配置文件 增加 proxy 代理配置

--- antd-manage-system/config-overrides.js
+++ antd-manage-system/config-overrides.js
@@ -1,4 +1,10 @@
-const { override, fixBabelImports, addLessLoader, addWebpackAlias, addWebpackExternals } = require('customize-cra');
+const {
+    override,
+    fixBabelImports,
+    addLessLoader,
+    addWebpackAlias,
+    disableEsLint
+} = require('customize-cra');
 const path = require("path");
 const themeColor = {
     "@primary-color": "#7b9dcc",
@@ -8,41 +14,58 @@ const themeColor = {
     "@link-color": "#2e27cc",
 };
 
-module.exports = override(
-    //写了下面这个部分，就实现了按需加载，再也不需要再每个页面里面都引入“antd/dist/antd.css”啦
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true  //这里一定要写true，css和less都不行哦
-    }),
+// 配置代理
+const addProxy = () => (configFunction) => {
+    configFunction.proxy = {
+        '/api': {
+            target: 'https://www.baidu.com',
+            changeOrigin: true,
+            pathRewrite: { '^/api': '/' },
+        },
+    };
 
-    addLessLoader({
-        javascriptEnabled: true,
-        //下面这行很特殊，这里是更改主题的关键，这里我只更改了主色，当然还可以更改其他的，下面会详细写出。
-        modifyVars: themeColor
-    }),
+    return configFunction;
+};
 
-    addWebpackAlias({ //路径别名
-        '@': path.resolve(__dirname, 'src'),
-    }),
+module.exports = {
+    webpack: override(
+        // 按需加载 antd
+        fixBabelImports('import', {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: true
+        }),
 
-    addWebpackExternals({ //不做打包处理配置，如直接以cdn引入的
-        // echarts: "window.echarts",
-        // highcharts:"window.highcharts"
-    }),
+        // 自定义 antd 主题
+        addLessLoader({
+            javascriptEnabled: true,
+            modifyVars: themeColor
+        }),
 
-    (config) => {
-        //修改、添加loader 配置 :
-        // 所有的loaders规则是在config.module.rules(数组)的第二项
-        // 即：config.module.rules[2].oneof  (如果不是，具体可以打印 一下是第几项目)
-        // 修改 less 配置 ，规则 loader 在第7项(具体可以打印配置)
-        const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf;
-        loaders[7].use.push({
-            loader: 'style-resources-loader',
-            options: {
-                patterns: path.resolve(__dirname, 'src/style/common.less')//全局引入公共的 less 文件
-            }
-        });
-        return config
-    }
-);
+        // 配置别名
+        addWebpackAlias({
+            '@': path.resolve(__dirname, 'src'),
+        }),
+
+        // 全局引入公共的 less 文件
+        (config) => {
+            //修改、添加loader 配置 :
+            // 所有的loaders规则是在config.module.rules(数组)的第二项
+            // 即：config.module.rules[2].oneof  (如果不是，具体可以打印 一下是第几项目)
+            // 修改 less 配置 ，规则 loader 在第7项(具体可以打印配置)
+            const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf;
+            loaders[7].use.push({
+                loader: 'style-resources-loader',
+                options: {
+                    patterns: path.resolve(__dirname, 'src/style/common.less')
+                }
+            });
+            return config
+        },
+
+        // webpack 中禁用 eslint
+        disableEsLint(),
+    ),
+
+    devServer: addProxy(),
+};
